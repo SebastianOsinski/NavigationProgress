6419fafedb006369af800f5a888348429d7477e8
Sebastian Osiński <seb.osinski@gmail.com>
2019-05-01 19:38:58 +0200

    Initial commit

---
+++ NavigationProgress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationProgress
+//
+//  Created by Sebastian Osiński on 01/05/2019.
+//  Copyright © 2019 Sebastian Osiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
